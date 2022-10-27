fd17c7fadc3fbc58d369098f0caed759e56da6ec
LeVanToan <dev10.vndigitech@gmail.com>
2022-10-27 23:25:25 +0700

    authen: update port

--- src/main/java/com/example/kltn/SpringAPILambdaBuy/security/config/SecurityConfig.java
+++ src/main/java/com/example/kltn/SpringAPILambdaBuy/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests()
-				.antMatchers("/api/authentication/**")
+				.antMatchers("/api/**")
 				.permitAll()
 			.anyRequest()
 			.authenticated();
